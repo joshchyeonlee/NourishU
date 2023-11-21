ccdc6d02a5c57e1a03e5db2adb24d7a20a34f01c
joshchyeonlee <joshchyeonlee@gmail.com>
2023-11-21 12:35:35 -0700

    clean up data

--- server/devtools/dummyAdminData.js
+++ server/devtools/dummyAdminData.js
@@ -13,9 +13,9 @@ AdminEmail  VARCHAR(255)     NOT NULL,
 PRIMARY KEY(AdminID),
 UNIQUE(AdminEmail));`;
 
-const admin1 = `INSERT INTO ADMIN(AdminID, AdminName, AdminEmail) VALUES(0, "Josh (Admin)", "josh@nourishu.com")`
-const admin2 = `INSERT INTO ADMIN(AdminID, AdminName, AdminEmail) VALUES(1, "Josh1 (Admin)", "josh1@nourishu.com")`
-const admin3 = `INSERT INTO ADMIN(AdminID, AdminName, AdminEmail) VALUES(2, "Josh2 (Admin)", "josh2@nourishu.com")`
+const admin1 = `INSERT INTO ADMIN(AdminID, AdminName, AdminEmail) VALUES(0, "Josh", "josh@nourishu.com")`
+const admin2 = `INSERT INTO ADMIN(AdminID, AdminName, AdminEmail) VALUES(1, "Samantha", "samantha@nourishu.com")`
+const admin3 = `INSERT INTO ADMIN(AdminID, AdminName, AdminEmail) VALUES(2, "Akansha", "akansha@nourishu.com")`
 
 const data = [admin1, admin2, admin3];
 
--- server/devtools/dummyGoalData.js
+++ server/devtools/dummyGoalData.js
@@ -20,22 +20,22 @@ FOREIGN KEY(UserID) REFERENCES USER(UserID) ON UPDATE CASCADE);`;
 
 const user0Goal = `
 INSERT INTO GOAL(UserID, GoalMilestone, GoalID, GoalName, GoalDuration, GoalProgress)
-VALUES(0, 1, 0, "get ripped", 10, "In Progress");`;
+VALUES(0, 1, 0, "gain muscle", 10, "In Progress");`;
 const user1Goal = `
 INSERT INTO GOAL(UserID, GoalMilestone, GoalID, GoalName, GoalDuration, GoalProgress)
-VALUES(1, 1, 1, "get shredded", 1, "In Progress");`;
+VALUES(1, 1, 1, "get leaner", 1, "In Progress");`;
 const user2Goal = `
 INSERT INTO GOAL(UserID, GoalMilestone, GoalID, GoalName, GoalDuration, GoalProgress)
 VALUES(2, 1, 2, "eat more", 3, "In Progress");`;
 const user3Goal = `
 INSERT INTO GOAL(UserID, GoalMilestone, GoalID, GoalName, GoalDuration, GoalProgress)
-VALUES(3, 1, 3, "eat bean", 4, "In Progress");`;
+VALUES(3, 1, 3, "eat more proteins", 4, "In Progress");`;
 const user4Goal = `
 INSERT INTO GOAL(UserID, GoalMilestone, GoalID, GoalName, GoalDuration, GoalProgress)
-VALUES(4, 1, 4, "eat cheese", 20, "In Progress");`;
+VALUES(4, 1, 4, "eat less carbs", 20, "In Progress");`;
 const user4Goal1 = `
 INSERT INTO GOAL(UserID, GoalMilestone, GoalID, GoalName, GoalDuration, GoalProgress)
-VALUES(4, 1, 5, "rip bean", 10, "In Progress");`;
+VALUES(4, 1, 5, "lose weight", 10, "Completed");`;
 
 const data = [user0Goal, user1Goal, user2Goal, user3Goal, user4Goal, user4Goal1];
 
