c2ba98175a09ee16d11d341abb530461c23b4614
joshchyeonlee <joshchyeonlee@gmail.com>
2023-11-23 17:28:06 -0700

    add overall rating

--- client/src/pages/ViewRecipe.js
+++ client/src/pages/ViewRecipe.js
@@ -14,8 +14,11 @@ const ViewRecipe = () => {
     const [recipeDifficulty, setRecipeDifficulty] = useState(0);
     const [recipeDescription, setRecipeDescription] = useState("");
     const [reviews, setReviews] = useState([]);
-    const [ratingDifficulty, setRatingDifficulty] = useState(0);
+    const [userRating, setUserRating] = useState(0);
     const [hover, setHover] = useState(-1);
+    const [userReview, setUserReview] = useState("");
+    const [userReviewLength, setUserReviewLength] = useState(0);
+    const [overallRating, setOverallRating] = useState(0);
 
     const fetchRecipe = async () => {
         const rID = {
@@ -53,18 +56,27 @@ const ViewRecipe = () => {
         try{
             const res = await axios.post("http://localhost:3001/getRecipeReviews", rID);
             setReviews(res.data);
+            parseReviewRatings(res.data);
         } catch (err) {
             throw(err);
         }
     }
 
+    const parseReviewRatings = (ratings) => {
+        const initVal = 0;
+        const sum = ratings.reduce((acc, val) => acc + val.Rdifficulty, initVal);
+        const avg = Math.round(sum / ratings.length * 10) / 10;
+
+        setOverallRating(avg);
+    }
+
     //reuse rating component from previous pr
     const setColor = (rating) => {
-        if(rating === 1) return {color: "blue"};
-        if(rating === 2) return {color: "green"};
-        if(rating === 3) return {color: "yellow"};
-        if(rating === 4) return {color: "orange"};
-        if(rating === 5) return {color: "red"};
+        if(rating <= 1) return {color: "blue"};
+        if(rating <= 2) return {color: "green"};
+        if(rating <= 3) return {color: "yellow"};
+        if(rating <= 4) return {color: "orange"};
+        if(rating <= 5) return {color: "red"};
     }
 
     const setRatingColor = () => {
@@ -75,12 +87,17 @@ const ViewRecipe = () => {
             if(hover === 4) return {color: "orange"};
             if(hover === 5) return {color: "red"};
         } else {
-            if(ratingDifficulty === 1) return {color: "blue"};
-            if(ratingDifficulty === 2) return {color: "green"};
-            if(ratingDifficulty === 3) return {color: "yellow"};
-            if(ratingDifficulty === 4) return {color: "orange"};
-            if(ratingDifficulty === 5) return {color: "red"};
+            if(userRating === 1) return {color: "blue"};
+            if(userRating === 2) return {color: "green"};
+            if(userRating === 3) return {color: "yellow"};
+            if(userRating === 4) return {color: "orange"};
+            if(userRating === 5) return {color: "red"};
         }
+    }
+
+    const handleReview = (review) => {
+        setUserReview(review);
+        setUserReviewLength(review.length);
     }
 
     //remove useEffect if passing in recipe information from previous page
@@ -104,6 +121,16 @@ const ViewRecipe = () => {
                         value={recipeDifficulty}
                         icon={<LocalDiningIcon sx={setColor(recipeDifficulty)}/>}
                         emptyIcon={<LocalDiningIcon/>}
+                        readOnly
+                    />
+                </Box>
+                <Box display="flex" justifyContent="space-between" padding={1} sx={{width:"30%"}}>
+                    <Typography>Overall Rating</Typography>
+                    <Rating
+                        value={overallRating}
+                        icon={<FavoriteIcon sx={setColor(overallRating)}/>}
+                        emptyIcon={<FavoriteIcon/>}
+                        precision={0.1}
                         readOnly
                     />
                 </Box>
@@ -166,10 +193,10 @@ const ViewRecipe = () => {
                                     <Box display="flex" justifyContent="space-between" paddingBottom={1}>
                                         <Typography>Rating </Typography>
                                         <Rating
-                                            value={ratingDifficulty}
+                                            value={userRating}
                                             icon={<FavoriteIcon sx={setRatingColor()}/>}
                                             emptyIcon={<FavoriteIcon/>}
-                                            onChange={(event) => setRatingDifficulty(parseInt(event.target.value))}
+                                            onChange={(event) => setUserRating(parseInt(event.target.value))}
                                             onChangeActive={(event, newHover) => setHover(newHover)}
                                         />
                                     </Box>
@@ -177,9 +204,14 @@ const ViewRecipe = () => {
                                         sx={{width:"100%"}}
                                         multiline
                                         rows={4}
-                                        />
-                                    <Box paddingTop={1} display="flex" justifyContent="flex-end">
-                                        <Button variant="contained">Add Review</Button>
+                                        inputProps={{ maxLength: 255 }}
+                                        onChange={(event) => handleReview(event.target.value)}
+                                    />
+                                    <Box padding={1} display="flex" justifyContent="space-between">
+                                        <Typography variant="caption">{userReviewLength}/255</Typography>
+                                        <Box display="flex" justifyContent="flex-end">
+                                            <Button variant="contained">Add Review</Button>
+                                        </Box>
                                     </Box>
                                 </Box>
                             </Box>
