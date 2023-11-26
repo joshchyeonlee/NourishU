cda41e0f6a354df2a40424484a0c72fbb2c6798e
samanthaaliu <samantha.liu10@gmail.com>
2023-11-26 01:03:38 -0700

    Update CookingConfidence.js

--- client/src/pages/CookingConfidence.js
+++ client/src/pages/CookingConfidence.js
@@ -11,27 +11,25 @@ const CookingConfidence = () => {
         <Box padding={10}>
           <Typography variant="h4">How Confident are you with cooking?</Typography>
         </Box>
-        <Grid container spacing={1} direction="column" width={200} justifyContent="flex-start" textAlign="left">
-          <Grid item>
-            <Box sx={{ width: 300}}>
-            <Slider
-              aria-label="Cooking Confidence"
-              defaultValue={30}
-              getAriaValueText={valuetext}
-              valueLabelDisplay="auto"
-              step={1}
-              marks
-              min={1}
-              max={10}
-              sx={{ width: '200%', mt: 4 }}
-            />
-            </Box>
-          </Grid>
-            <Grid item>
-                <Button variant="contained" sx ={{width: '200px'}}>Continue</Button>
-            </Grid>
-        </Grid>
+      <Box sx={{ width: 300, marginLeft: '80', marginRight: '80', textAlign: 'center', }}>
+        <Slider
+          aria-label="Cooking Confidence"
+          defaultValue={30}
+          getAriaValueText={valuetext}
+          valueLabelDisplay="auto"
+          step={1}
+          marks
+          min={1}
+          max={10}
+          sx={{ width: '200%', mt: 4 }}
+        />
       </Box>
+      <Grid container spacing={3} direction="column" width={200}>
+        <Grid item>
+            <Button variant="contained" sx={{width: '200px'}}>Continue</Button>
+        </Grid>
+      </Grid>
+    </Box>
     </div>
   );
 };
