42e93955b9ad5bbd9db1c548374cdc825352294b
joshchyeonlee <joshchyeonlee@gmail.com>
2023-12-02 02:50:20 -0700

    add logo and favicon

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,13 +1,12 @@
-import { Typography, Button, Box } from "@mui/material";
+import { Button, Box } from "@mui/material";
 import { Link } from "react-router-dom";
+import logo_bg from '../assets/logo_bg.png';
 
 const Welcome = () => {
     return (
-        <Box padding={4} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-            <Box padding={8} display="flex" justifyContent="center">
-                <Typography variant="h4">{"NourishU"}</Typography>
-            </Box>
-            <Box paddingTop={4} display="flex" justifyContent="center" flexDirection="column">
+        <Box padding={6} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+            <img src={logo_bg} alt="NourishU"/>
+            <Box display="flex" justifyContent="center" flexDirection="column">
                 <Box padding={1}>
                     <Button variant="contained" sx = {{width: '200px'}} component={Link} to={{pathname:"/login"}}>Log in</Button>
                 </Box>
