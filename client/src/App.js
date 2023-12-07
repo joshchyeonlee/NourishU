f7cfef32617c30915eb26e0e4a54cc4d47b17f61
joshchyeonlee <joshchyeonlee@gmail.com>
2023-12-07 12:22:00 -0700

    missed this merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,6 @@ function App() {
       <ThemeProvider theme={theme}> 
       <CssBaseline />
       <Routes>
-<<<<<<< HEAD
         <Route path="/dashboard" element={<RequireAuth loginPath='/'><Dashboard /></RequireAuth>}/>
         <Route path="/recipes" element={<RequireAuth loginPath='/'><Recipes /></RequireAuth>}/>
         <Route path="/profile" element={<RequireAuth loginPath='/'><Profile/></RequireAuth>}/>
@@ -58,37 +57,10 @@ function App() {
         <Route path="/logMeal" element={<RequireAuth loginPath='/'><LogMeal/></RequireAuth>}/>
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
-        <Route path ="/signup-initial" element={<SignUpInitial />} />
-        <Route path ="/signup-info" element={<SignUpInfo />} />
-        <Route path ="/signup-userinterests" element={<UserInterests />} />
-        <Route path="/cookingConfidence" element={<CookingConfidence/>}/>
-=======
-        <Route path="/dashboard" element={<RequireAuth loginPath='/welcome'><Dashboard /></RequireAuth>}/>
-        <Route path="/recipes" element={<RequireAuth loginPath='/welcome'><Recipes /></RequireAuth>}/>
-        <Route path="/profile" element={<RequireAuth loginPath='/welcome'><Profile/></RequireAuth>}/>
-        <Route path="/searchRecipes" element={<RequireAuth loginPath='/welcome'><SearchRecipes/></RequireAuth>}/>
-        <Route path="/viewMeal" element={<RequireAuth loginPath='/welcome'><ViewMeal/></RequireAuth>}/>
-        <Route path="/editMeal" element={<RequireAuth loginPath='/welcome'><EditMeal/></RequireAuth>}/>
-        <Route path="/editFood" element={<RequireAuth loginPath='/welcome'><EditFood/></RequireAuth>}/>
-        <Route path="/cookingConfidence" element={<CookingConfidence />}/>
-        <Route path="/createRecipe" element={<RequireAuth loginPath='/welcome'><CreateRecipe /></RequireAuth>}/>
-        <Route path="/setIngredients" element={<RequireAuth loginPath='/welcome'><SetIngredients/></RequireAuth>}/>
-        <Route path="/setRecipeInstructions" element={<RequireAuth loginPath='/welcome'><SetRecipeInstructions/></RequireAuth>}/>
-        <Route path="/viewRecipe" element={<RequireAuth loginPath='/welcome'><ViewRecipe/></RequireAuth>}/>
-        <Route path="/dashboard" element={<RequireAuth loginPath='/welcome'><Dashboard/></RequireAuth>}/>
-        <Route path="/editRecipe" element={<RequireAuth loginPath='/welcome'><EditRecipeRedirect /></RequireAuth>} />
-        <Route path="/editRecipeDetails" element={<RequireAuth loginPath='/welcome'><EditRecipe /></RequireAuth>} />
-        <Route path="/editRecipeSteps" element={<RequireAuth loginPath='/welcome'><EditRecipeSteps /></RequireAuth>} />
-        <Route path="/editRecipeIngredients" element={<RequireAuth loginPath='/welcome'><EditRecipeIngredients /></RequireAuth>} />
-        <Route path="/viewYourRecipes" element={<RequireAuth loginPath='/welcome'><YourRecipes /></RequireAuth>} />
-        <Route path="/logMeal" element={<RequireAuth loginPath='/welcome'><LogMeal/></RequireAuth>}/>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/signup-initial" element={<SignUpInitial />} />
         <Route path="/signup-info" element={<SignUpInfo />} />
         <Route path="/signup-userinterests" element={<UserInterests />} />
         <Route path="*" element={<Navigate replace to="/dashboard" />} />
->>>>>>> main
     </Routes>
     </ThemeProvider>
   </BrowserRouter>
